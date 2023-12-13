8e47b5e19c9d6c2f9cb4d7709b37f50851a0c112
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2023-12-13 13:29:36 +0200

    open

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css';
 
-const Modal = ({ image, onClose }) => (
-  <div className="overlay" onClick={onClose}>
-    <div className="modal">
-      <img src={image.largeImageURL} alt={image.id} />
+const Modal = ({ image, onClose }) => {
+  const [lightboxIsOpen, setLightboxIsOpen] = useState(true);
+
+  useEffect(() => {
+    const handleKeyPress = e => {
+      if (e.key === 'Escape') {
+        setLightboxIsOpen(false);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  const images = [image.largeImageURL];
+  const currentIndex = 0;
+
+  const handleClose = () => {
+    setLightboxIsOpen(false);
+    onClose();
+  };
+
+  return (
+    <div>
+      {lightboxIsOpen && (
+        <Lightbox
+          mainSrc={images[currentIndex]}
+          nextSrc={images[(currentIndex + 1) % images.length]}
+          prevSrc={images[(currentIndex + images.length - 1) % images.length]}
+          onCloseRequest={handleClose}
+          onMovePrevRequest={() => setLightboxIsOpen(true)}
+          onMoveNextRequest={() => setLightboxIsOpen(true)}
+        />
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
